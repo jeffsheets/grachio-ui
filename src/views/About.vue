63d965720d5ae1f9267d4c0fa61a10a734015fd1
Jeff Sheets <jeffsheets@gmail.com>
2018-09-21 13:44:50 -0500

    move image

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page just to show another route</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <GrachioDisplay :grachio="grachio"/>
   </div>
 </template>
